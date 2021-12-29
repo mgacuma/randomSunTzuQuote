10ca0b9ee424eef842f2c295f22d300889a2ffd4
Miguel Gacuma <mgacuma@mail.sfsu.edu>
2021-12-29 02:11:15 -0800

    initial commit

---
+++ mixed.js
@@ -0,0 +1,13 @@
+let messages = [
+    'Appear weak when you are strong, and strong when you are weak.',
+    'Even the finest sword plunged into salt water will eventually rust.',
+    'Let your plans be dark and impenetrable as night, and when you move, fall like a thunderbolt.',
+    "Supreme excellence consists of breaking the enemy's resistance without fighting.",
+    'In the midst of chaos, there is also opportunity',
+    'Victorious warriors win first and then go to war, while defeated warriors go to war first and then seek to win',
+    'The greatest victory is that which requires no battle.',
+    'To know your Enemy, you must become your Enemy.',
+    'There is no instance of a nation benefitting from prolonged warfare.',
+]
+
+console.log(messages[Math.floor(Math.random() * 10)])
